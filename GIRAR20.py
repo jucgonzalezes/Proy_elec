d14a522f958393231a8a1556f151c762ab7788de
Rivas Sebastian <srivasb@unal.edu.co>
2019-06-19 10:31:38 -0500

    actualización con movimiento de los 3 motores

--- GIRAR20.py
+++ GIRAR20.py
@@ -10,20 +10,55 @@ def rot(Motor,Sentido, step_count):
 			GPIO.output(eval('STEP{}'.format(Motor)),elem)
 			sleep(delay)
 		GPIO.output(eval('LED{}'.format(Motor)),GPIO.HIGH) if x < step_count-1 else GPIO.output(eval('LED{}'.format(Motor)),GPIO.LOW)
+
+	
 #DECLARACION DE VARIABLES DE PARA LOS MOTORES--------------------------------------------------
-CW, CCW=1, 0	#rotacion en direccion contraria (del) del reloj
+CW, CCW=0, 1	#rotacion en direccion contraria (del) del reloj
 DIR1, DIR2, DIR3 = "P8_7","P8_13","P8_13" # Puertos de direccion
 STEP1,STEP2,STEP3 = "P8_9","P8_11","P8_11" # Puertos de paso
 LED1, LED2, LED3 = "P8_14","P8_16","P8_16" # Puertos LED
-#Inicializacion de los puertos de salida
+
 for elm in np.arange(1,4):
 	A=['DIR{}'.format(elm), 'STEP{}'.format(elm), 'LED{}'.format(elm)]
 	for i in range(len(A)):
 		GPIO.setup(eval(A[i]), GPIO.OUT)
+		
+				
+#PARA EL PUNTO INICAL DE LOS MOTORES___________________________________________________________	
+	
+volver_punto_inicial=open("punto_inicial.dat","r")
+delay = 0.001#input('Ingrese delay: ')		# tiempo que se demora en dar un paso	
+#for line,i in zip(volver_punto_inicial,np.arange(1,4)):
+#	coordenada_inicial = eval(volver_punto_inicial.readline())
+#	rot("{}".format(i),CCW, coordenada_inicial)	
+#	
+volver_punto_inicial.close()	
+
+	
+	
+		
+		
 #SENTIDO Y PASO DE LOS MOTORES-------------------------------------------------------------------
-SPR = input("Ingrese numero de pasos (200 pasos por vuelta): ")  #pasos por revolucion
-Motor = input('Ingrese el motor a mover (1,2 o 3): ')
-delay = 0.001#input('Ingrese delay: ')		# tiempo que se demora en dar un paso
+
+SPR1 = input("Ingrese numero de pasos para el motor 1 (200 pasos por vuelta): ")  #pasos por revolucion
+SPR2 = input("Ingrese numero de pasos para el motor 2 (200 pasos por vuelta): ")  #pasos por revolucion
+SPR3 = input("Ingrese numero de pasos para el motor 3 (200 pasos por vuelta): ")  #pasos por revolucion
+
+
+Dir = input('Direccion (CW o CCW): ')
+
+rot("1",Dir,SPR1)      
+rot("2",Dir,SPR2)  
+rot("3",Dir,SPR3)  
+
+
+puntocero=open("puntos_iniciales.dat","w")
+puntocero.write("{}\n,{}\n,{}".format(SPR1,SPR2,SPR3))
+puntocero.close()
+
+
+
 #ACTIVAR LA FUNCION DE ROTACION DE MOTORES-------------------------------------------------------
-rot(Motor, CW, SPR)
 GPIO.cleanup()
+
+
